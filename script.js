31be66eb6ea102e2ab53053fadef073a21a6c89e
Mike Barkas <mike@barkas.com>
2014-09-14 22:12:58 -0400

    red class. needs fixin.

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
 var secondsRemaining;
 var intervalHandle;
 
-// Show the form again.
+/*
+ *  Show the form again.
+ */
 function resetPage () {
   document.getElementById('inputArea').style.display = 'block';
 }
 
-
+/*
+ * Seconds interval timer.
+ */
 function tick () {
   var timeDisplay = document.getElementById('time');
 
+  // Create minues and seconds.
   var min = Math.floor(secondsRemaining / 60);
   var sec = secondsRemaining - (min * 60);
 
+  // Add leading 0.
   if (sec < 10) {
     sec = '0' + sec;
   }
 
-  var message = min.toString() + ':' + sec;
+  // Change color to red.
+  if (min === 0 && sec < 11) {
+    document.getElementById('time').setAttribute('class', 'red');
+  }
 
+  // Create time display.
+  var message = min.toString() + ':' + sec;
   timeDisplay.innerHTML = message;
 
+  // Finish.
   if (secondsRemaining === 0) {
     alert('Done!');
     clearInterval(intervalHandle);
     resetPage();
   }
 
+  // Remove last.
   secondsRemaining--;
 }
 
+/*
+ * Countdown function.
+ */
 function startCountdown () {
   // Get minutes entered.
   var minutes = document.getElementById('minutes').value;
@@ -50,7 +66,9 @@ function startCountdown () {
   document.getElementById('inputArea').style.display = 'none';
 }
 
-// When page loads.
+/*
+ * When page loads.
+ */
 window.onload = function () {
   // Create text input.
   var inputMinutes = document.createElement('input');
