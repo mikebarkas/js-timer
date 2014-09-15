73643b91cae998e28b76fe0f3aaa880ce6f655df
Mike Barkas <mike@barkas.com>
2014-09-14 21:39:24 -0400

    js

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
+function startCountdown () {
+  // Get minutes entered.
+  var minutes = document.getElementById('minutes').value;
+  
+  // Check if not a number.
+  if (isNaN(minutes)) {
+    alert('Please enter a number.');
+  }
+
+  // Calculate seconds.
+  secondsRemaining = minutes * 60;
+
+  // Interval the tick function.
+  intervalHandle = setInterval(tick, 1000);
+
+  // Hide the form.
+  document.getElementById('inputArea').style.display = 'none';
+}
+
 
 // When page loads.
 window.onload = function () {
   // Create text input.
   var inputMinutes = document.createElement('input');
-  inputMinutes.setAttributes('id', 'minutes');
-  inputMinutes.setAttributes('type', 'text');
+  inputMinutes.setAttribute('id', 'minutes');
+  inputMinutes.setAttribute('type', 'text');
 
   // Create button.
   var startButton = document.createElement('input');
-  startButton.setAttributes('type', 'button');
-  startButton.setAttributes('value', 'Start Timer');
+  startButton.setAttribute('type', 'button');
+  startButton.setAttribute('value', 'Start Timer');
   // Button callback.
   startButton.onclick = function () {
     startCountdown();
   };
+
+  // Add elements to DOM.
+  document.getElementById('inputArea').appendChild(inputMinutes);
+  document.getElementById('inputArea').appendChild(startButton);
 };
